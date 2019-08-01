0c3ac6b2ca4d95066dc8ab7e01674d1e5ca74dbc
Nick Joyce <brawlyrox@gmail.com>
2019-08-01 07:56:26 +0100

    Set up new pages with templates and put the router together

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+const AddExpensePage = () => (
+    <div>This is from my add expense component</div>
+)
+
+export default AddExpensePage
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const EditExpensePage = (props) => {
+
+return <div>Editing the expense with the id of {props.match.params.id}</div>
+}
+
+export default EditExpensePage
