a246cd247a34f56a6ea8385503998469cb0998ce
Nick Joyce <brawlyrox@gmail.com>
2019-08-09 18:43:26 +0100

    Connected store to my react app. Started making it interactive, adding ability to change search filter via input and remove items

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
@@ -16,12 +18,26 @@ const store = configureStore()
 const expenseOne = store.dispatch(addExpense({description: 'Water bill', amount: 100, createdAt: -21000}))
 const expenseTwo = store.dispatch(addExpense({ description: "Gas bill", amount: 200, createdAt: -1000 }))
 
+
 store.dispatch(setTextFilter('water'))
 
 const state = store.getState()
+
+// visibleExpenses takes 2 arguments, one is expenses and the other is filters, both available from state
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
-console.log(visibleExpenses)
+// setTimeout(() => {
+//     store.dispatch(setTextFilter('bill'))
+// }, 3000);
 
+// console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+    
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './ExpenseListFilters'
+
 
 const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
+  <div>
+  <ExpenseListFilters />
+  <ExpenseList />
+  </div>
 )
 
 export default ExpenseDashboardPage
