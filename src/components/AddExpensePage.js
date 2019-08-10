d4c9a12edbe401544f587c2e6d98daa935e4cae0
Nick Joyce <brawlyrox@gmail.com>
2019-08-10 14:20:27 +0100

    create add expense form with controlled components. Add regex to validate amount input

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,13 @@
 import React from "react"
-import ReactDOM from "react-dom"
+
+import ExpenseForm from './ExpenseForm'
 
 
 const AddExpensePage = () => (
-    <div>This is from my add expense component</div>
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
 )
 
 export default AddExpensePage
