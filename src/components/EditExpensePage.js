9bfd828c6a5d4c4d0a57cb8e65f286b729de222c
Nick Joyce <brawlyrox@gmail.com>
2019-08-11 09:44:42 +0100

    Add the ability to edit an expense by pulling in the ID and filtering the expense from the store to match ID

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,39 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { connect } from 'react-redux'
+
+import ExpenseForm from './ExpenseForm'
+import { editExpense, removeExpense } from '../actions/expenses'
 
 const EditExpensePage = (props) => {
 
-return <div>Editing the expense with the id of {props.match.params.id}</div>
+
+return (
+  <div>
+    <ExpenseForm
+      expense={props.expense}
+      onSubmit={expense => {
+        props.dispatch(editExpense(props.expense.id, expense))
+        props.history.push("/")
+      }}
+    />
+    <button
+      onClick={e => {
+        props.dispatch(removeExpense({id: props.expense.id}))
+        props.history.push("/")
+      }}
+    >
+      Remove
+    </button>
+  </div>
+)
 }
 
-export default EditExpensePage
+const mapStatetoProps = (state, props) => {
+    return {
+      expense: state.expenses.find(
+        expense => expense.id === props.match.params.id
+      )
+    }
+}
+
+export default connect(mapStatetoProps)(EditExpensePage)
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-import { removeExpense } from '../actions/expenses'
+import { Link } from 'react-router-dom'
 
 
 // description, amount, createdAt
-const ExpenseListItem = ({dispatch, description, amount, createdAt, id}) => (
+const ExpenseListItem = ({ description, amount, createdAt, id}) => (
   <div>
-    <h3>Description:  {description} </h3>
+  <Link to={`/edit/${id}`}>
+    <h3>{description}</h3>
+  </Link>
     <p>Amount: {amount} </p>
     <p>Created At: {createdAt} </p>
-    <button onClick={(e) => {
-      dispatch(removeExpense({id}))
-    }}>Remove</button>
+   
+    
   </div>
 )
 
 
-
-export default connect()(ExpenseListItem)
+export default ExpenseListItem
 
