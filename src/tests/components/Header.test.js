9ca585d52944e7979f1192fcf82fa5c86069d353
Nick Joyce <brawlyrox@gmail.com>
2019-08-21 19:51:49 +0100

    setup Enzyme to do component snapshot testing. Made sure to use toJSON serializer

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Header from '../../components/Header'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
+
 
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer()
-
-    renderer.render(<Header />)
-
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
+    const wrapper = shallow(<Header />)
+    expect(wrapper).toMatchSnapshot()
 
 })
 
